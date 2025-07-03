951daa07b0b26eb0fbd41afd609ff39b1f963ea7
shafayat <shafayat1777@gmail.com>
2025-07-04 05:02:54 +0600

    ci: :sparkles: Added new featured

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+export { zodResolver } from "@hookform/resolvers/zod"
+export { useForm } from "react-hook-form"
+export { z } from 'zod';
+
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+
